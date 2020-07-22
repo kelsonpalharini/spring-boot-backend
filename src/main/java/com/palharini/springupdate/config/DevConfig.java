06aad8187702d2773d35472f0ebadbc7e52b9875
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-07-22 18:32:35 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/palharini/springupdate/config/DevConfig.java
+++ src/main/java/com/palharini/springupdate/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.palharini.springupdate.services.DBService;
+import com.palharini.springupdate.services.EmailService;
+import com.palharini.springupdate.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,6 +32,10 @@ public class DevConfig {
 		dbService.instantiateTestDatabase();
 		
 		return true;
+	}
+	
+	@Bean public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
