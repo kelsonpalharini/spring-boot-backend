72d53eb47abd05629fda3d5156fa1f45681644f2
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-07-22 17:34:51 -0300

    Implementando toString de Pedido

--- src/main/java/com/palharini/springupdate/domain/Pedido.java
+++ src/main/java/com/palharini/springupdate/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.palharini.springupdate.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -135,5 +138,27 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-	
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat stf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		builder.append("Pedido Número: ");
+		builder.append(getId());
+		builder.append(", Data: ");
+		builder.append(stf.format(getDataPedido()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for (ItemPedido ip: getItens()) {
+			builder.append(ip.toString());
+		}
+		builder.append("Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();		
+	}	
 }
