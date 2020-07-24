453764d1dbac76ce321dc8199e1ff23c8a10c6e2
Kelson Palharini <kelsonpalharini@bsd.com.br>
2020-07-24 02:23:26 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/palharini/springupdate/services/DBService.java
+++ src/main/java/com/palharini/springupdate/services/DBService.java
@@ -20,6 +20,7 @@ import com.palharini.springupdate.domain.PagamentoComCartao;
 import com.palharini.springupdate.domain.Pedido;
 import com.palharini.springupdate.domain.Produto;
 import com.palharini.springupdate.domain.enums.EstadoPagamento;
+import com.palharini.springupdate.domain.enums.Perfil;
 import com.palharini.springupdate.domain.enums.TipoCliente;
 import com.palharini.springupdate.repositories.CategoriaRepository;
 import com.palharini.springupdate.repositories.CidadeRepository;
@@ -124,14 +125,20 @@ public class DBService {
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "palharinik@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA, pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "palharini23@gmail.com", "3162832740", TipoCliente.PESSOA_FISICA, pe.encode("123"));
+		cli2.addPefil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("54645632", "34534637"));
+		
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "38777012", cli2, c2);
 
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
